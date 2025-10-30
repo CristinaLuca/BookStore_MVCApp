68d67aa6fb4cc675254146b6d3ea7c603119c08b
cl19 <cl19@ARU-F0G2YD3>
2025-10-30 20:36:39 +0000

    CRUD operations implemented

--- BookStore_MVCApp/Controllers/BooksController.cs
+++ BookStore_MVCApp/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
 ﻿using BookStore_MVCApp.Data;
+using BookStore_MVCApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,5 +26,114 @@ namespace BookStore_MVCApp.Controllers
         }
 
 
+        /*
+         * ADD BOOK
+         */
+        //the show form
+        [HttpGet]
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+
+        //adds a book to the database
+        [HttpPost]
+        public async Task<IActionResult> AddBook(Book book)
+        {
+
+            appDBcontext.Add(book);
+            await appDBcontext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        /* 
+ * DELETE
+ */
+        // GET: Books/Delete/XX
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var book = await appDBcontext.Books.FirstOrDefaultAsync(bookIterator => bookIterator.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            
+            return View(book); // show a conformation page
+        }
+
+        // POST: Books/Delete/XX
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var book = await appDBcontext.Books.FindAsync(id);
+
+            if (book != null)
+            {
+                appDBcontext.Books.Remove(book);
+                await appDBcontext.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index)); // back to list
+        }
+
+        /*
+ * UPDATE
+ */
+        // GET: Books/Edit/XX
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                TempData["ErrorMessage"] = " In the meantime, this book has been deleted.";
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            var book = await appDBcontext.Books.FindAsync(id);
+            if (book == null)
+            {
+                //what should be done here?
+
+            }
+
+            return View(book); // returns the edit form
+        }
+
+
+        // POST: Books/Edit/XX
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Book book)
+        {
+            if (id != book.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    appDBcontext.Update(book);
+                    await appDBcontext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    // what does happen if the book doesn’t exist??
+                    throw;
+                }
+            }
+
+            return View(book);
+        }
+
     }
+
+
 }
--- BookStore_MVCApp/Models/Book.cs
+++ BookStore_MVCApp/Models/Book.cs
@@ -1,10 +1,14 @@
-﻿namespace BookStore_MVCApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore_MVCApp.Models
 {
     public class Book
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+
+        [Range(1, 500, ErrorMessage = "Price must be between 1 and 500.")]
         public decimal Price { get; set; }
 
     }
